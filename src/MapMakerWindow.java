467db244789ee771b6312556edac63799e667a88
Chad Fraser <chad.fraser@mail.mcgill.ca>
2019-01-06 05:55:22 -0700

    Add subpanels to improve JFrame layout

--- src/MapMakerWindow.java
+++ src/MapMakerWindow.java
@@ -9,14 +9,15 @@ public class MapMakerWindow extends JFrame {
     private ImagePreviewPanel imagePreviewPanel;
     private DirectionalButtonPanel directionalButtonPanel;
 
+    private JPanel imagePreviewSubPanel;
+    private JPanel directionalButtonSubPanel;
     private JPanel directionalButtonAndImagePreviewPanel;
     private JTabbedPane optionTabbedPane;
 
     private MapMakerWindow() {
         setTitle("NES Map Maker");
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        setResizable(false);
-//        setResizable(true);
+        setResizable(true);
         setFocusable(true);
         setPreferredSize(new Dimension(WIDTH, HEIGHT));
         setMinimumSize(new Dimension(WIDTH, HEIGHT));
@@ -28,6 +29,8 @@ public class MapMakerWindow extends JFrame {
         directionalButtonPanel = new DirectionalButtonPanel(this);
 //        settingsPanel = new SettingsPanel(this);
 
+        imagePreviewSubPanel = new JPanel();
+        directionalButtonSubPanel = new JPanel();
         directionalButtonAndImagePreviewPanel = new JPanel();
 
         optionTabbedPane = new JTabbedPane(SwingConstants.RIGHT, JTabbedPane.SCROLL_TAB_LAYOUT);
@@ -48,73 +51,46 @@ public class MapMakerWindow extends JFrame {
         new MapMakerWindow();
     }
 
-//    public static int getConstantWidth() {
-//        return WIDTH;
-//    }
-//
-//    public static int getConstantHeight() {
-//        return HEIGHT;
-//    }
-
     private void initializeLayout() {
         setLayout(new BoxLayout(getContentPane(), BoxLayout.LINE_AXIS));
         add(mapMakerImagePanel);
 //        add(directionalButtonAndImagePreviewPanel);
         add(optionTabbedPane);
-//        setLayout(new GridBagLayout());
-//        GridBagConstraints c = new GridBagConstraints();
-//
-//        c.fill = GridBagConstraints.NONE;
-//        c.anchor = GridBagConstraints.LINE_START;
-//        c.insets = new Insets(5, 5, 5, 5);
-//        c.weightx = 0.5;
-//        c.weighty = 0.5;
-//        c.gridwidth = 1;
-//        c.gridheight = 1;
-//        c.gridx = 0;
-//        c.gridy = 0;
-//        add(mapMakerImagePanel, c);
-//
-//        c.weightx = 0.5;
-//        c.weighty = 0.5;
-//        c.gridwidth = 1;
-//        c.gridheight = 2;
-//        c.gridx = 1;
-//        c.gridy = 0;
-//        add(directionalButtonAndImagePreviewPanel, c);
-
-//        c.weightx = 0.5;
-//        c.weighty = 0.5;
-//        c.gridwidth = 1;
-//        c.gridheight = 1;
-//        c.gridx = 1;
-//        c.gridy = 2;
-//        add(Box.createGlue(), c);
-//
-//        c.weightx = 0.5;
-//        c.weighty = 0.5;
-//        c.gridwidth = 1;
-//        c.gridheight = 1;
-//        c.gridx = 1;
-//        c.gridy = 3;
-//        add(imagePreviewPanel, c);
     }
 
     private void initializePanels() {
+        initializeImagePreviewSubPanel();
+        initializeDirectionalButtonSubPanel();  // TODO: Figure out how to prevent this panel resizing immediately
         initializeDirectionalImagePreviewPanel();
+    }
+
+    private void initializeImagePreviewSubPanel() {
+        imagePreviewSubPanel.setLayout(new GridBagLayout());
+        GridBagConstraints c = new GridBagConstraints();
+
+        c.fill = GridBagConstraints.NONE;
+        imagePreviewSubPanel.add(imagePreviewPanel, c);
+    }
+
+    private void initializeDirectionalButtonSubPanel() {
+        directionalButtonSubPanel.setLayout(new GridBagLayout());
+        GridBagConstraints c = new GridBagConstraints();
+
+        c.fill = GridBagConstraints.NONE;
+        directionalButtonSubPanel.add(directionalButtonPanel, c);
     }
 
     private void initializeDirectionalImagePreviewPanel() {
         directionalButtonAndImagePreviewPanel.setLayout(new BoxLayout(directionalButtonAndImagePreviewPanel,
                                                                       BoxLayout.PAGE_AXIS));
-        directionalButtonAndImagePreviewPanel.add(directionalButtonPanel);
+        directionalButtonAndImagePreviewPanel.add(directionalButtonSubPanel);
 //        directionalButtonAndImagePreviewPanel.add(Box.createVerticalGlue());
         JSeparator separator = new JSeparator();
         separator.setBackground(Color.GRAY);
-//        directionalButtonAndImagePreviewPanel.add(Box.createRigidArea(new Dimension(5, 5)));
+        directionalButtonAndImagePreviewPanel.add(Box.createRigidArea(new Dimension(5, 5)));
         directionalButtonAndImagePreviewPanel.add(separator);
         directionalButtonAndImagePreviewPanel.add(Box.createRigidArea(new Dimension(5, 5)));
-        directionalButtonAndImagePreviewPanel.add(imagePreviewPanel);
+        directionalButtonAndImagePreviewPanel.add(imagePreviewSubPanel);
         directionalButtonAndImagePreviewPanel.setBorder(BorderFactory.createEmptyBorder(5, 5, 5, 5));
     }
 
