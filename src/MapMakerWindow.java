f716eb3892b73310b6202be43f73a32602ea51e4
Chad Fraser <chad.fraser@mail.mcgill.ca>
2019-01-06 19:47:08 -0700

    Add additional subpanels and simplify methods to prevent repetition. TODO: Remove imagePreviewPanel from side panels

--- src/MapMakerWindow.java
+++ src/MapMakerWindow.java
@@ -8,10 +8,19 @@ public class MapMakerWindow extends JFrame {
     private MapMakerImagePanel mapMakerImagePanel;
     private ImagePreviewPanel imagePreviewPanel;
     private DirectionalButtonPanel directionalButtonPanel;
+    private ZoomPanel zoomPanel;
+    private SavePanel savePanel;
+    private UndoButtonPanel undoButtonPanel;
 
     private JPanel imagePreviewSubPanel;
     private JPanel directionalButtonSubPanel;
     private JPanel directionalButtonAndImagePreviewPanel;
+    private JPanel zoomSubPanel;
+    private JPanel zoomAndImagePreviewPanel;
+    private JPanel saveSubPanel;
+    private JPanel saveAndImagePreviewPanel;
+    private JPanel undoSubPanel;
+    private JPanel undoAndImagePreviewPanel;
     private JTabbedPane optionTabbedPane;
 
     private MapMakerWindow() {
@@ -27,17 +36,25 @@ public class MapMakerWindow extends JFrame {
         mapMakerImagePanel = new MapMakerImagePanel(this);
         imagePreviewPanel = new ImagePreviewPanel(this);
         directionalButtonPanel = new DirectionalButtonPanel(this);
-//        settingsPanel = new SettingsPanel(this);
+        zoomPanel = new ZoomPanel(this);
+        savePanel = new SavePanel(this);
+        undoButtonPanel = new UndoButtonPanel(this);
 
         imagePreviewSubPanel = new JPanel();
         directionalButtonSubPanel = new JPanel();
         directionalButtonAndImagePreviewPanel = new JPanel();
+        zoomSubPanel = new JPanel();
+        zoomAndImagePreviewPanel = new JPanel();
+        saveSubPanel = new JPanel();
+        saveAndImagePreviewPanel = new JPanel();
+        undoSubPanel = new JPanel();
+        undoAndImagePreviewPanel = new JPanel();
 
         optionTabbedPane = new JTabbedPane(SwingConstants.RIGHT, JTabbedPane.SCROLL_TAB_LAYOUT);
         optionTabbedPane.addTab("EDIT", null, directionalButtonAndImagePreviewPanel, "Edit");
-        optionTabbedPane.addTab("ZOOM", null, new JPanel(), "Zoom");
-        optionTabbedPane.addTab("SAVE", null, new JPanel(), "Save/Open");
-        optionTabbedPane.addTab("UNDO", null, new JPanel(), "Undo/Redo");
+        optionTabbedPane.addTab("ZOOM", null, zoomAndImagePreviewPanel, "Zoom");
+        optionTabbedPane.addTab("SAVE", null, saveAndImagePreviewPanel, "Save/Open");
+        optionTabbedPane.addTab("UNDO", null, undoAndImagePreviewPanel, "Undo/Redo");
         optionTabbedPane.addTab("OPT.", null, new JPanel(), "Settings");
 
         initializePanels();
@@ -60,16 +77,16 @@ public class MapMakerWindow extends JFrame {
 
     private void initializePanels() {
         initializeImagePreviewSubPanel();
-        initializeDirectionalButtonSubPanel();  // TODO: Figure out how to prevent this panel resizing immediately
-        initializeDirectionalImagePreviewPanel();
-    }
+//        initializeDirectionalButtonSubPanel();  // TODO: Figure out how to prevent this panel resizing immediately
+        initializeSubPanel(directionalButtonSubPanel, directionalButtonPanel);
+        initializeSubPanel(zoomSubPanel, zoomPanel);
+        initializeSubPanel(saveSubPanel, savePanel);
+        initializeSubPanel(undoSubPanel, undoButtonPanel);
 
-    private void initializeImagePreviewSubPanel() {
-        imagePreviewSubPanel.setLayout(new GridBagLayout());
-        GridBagConstraints c = new GridBagConstraints();
-
-        c.fill = GridBagConstraints.NONE;
-        imagePreviewSubPanel.add(imagePreviewPanel, c);
+        initializeSidePanel(directionalButtonAndImagePreviewPanel, directionalButtonSubPanel);
+        initializeSidePanel(zoomAndImagePreviewPanel, zoomSubPanel);
+        initializeSidePanel(saveAndImagePreviewPanel, saveSubPanel); 
+        initializeSidePanel(undoAndImagePreviewPanel, undoSubPanel);
     }
 
     private void initializeDirectionalButtonSubPanel() {
@@ -80,18 +97,49 @@ public class MapMakerWindow extends JFrame {
         directionalButtonSubPanel.add(directionalButtonPanel, c);
     }
 
-    private void initializeDirectionalImagePreviewPanel() {
-        directionalButtonAndImagePreviewPanel.setLayout(new BoxLayout(directionalButtonAndImagePreviewPanel,
-                                                                      BoxLayout.PAGE_AXIS));
-        directionalButtonAndImagePreviewPanel.add(directionalButtonSubPanel);
+    private void initializeImagePreviewSubPanel() {
+        imagePreviewSubPanel.setLayout(new GridBagLayout());
+        GridBagConstraints c = new GridBagConstraints();
+
+        c.fill = GridBagConstraints.NONE;
+        imagePreviewSubPanel.add(imagePreviewPanel, c);
+    }
+
+    private void initializeZoomSubPanel() {
+        zoomSubPanel.setLayout(new GridBagLayout());
+        GridBagConstraints c = new GridBagConstraints();
+
+        c.fill = GridBagConstraints.NONE;
+        zoomSubPanel.add(zoomPanel, c);
+    }
+
+    private void initializeSaveSubPanel() {
+        saveSubPanel.setLayout(new GridBagLayout());
+        GridBagConstraints c = new GridBagConstraints();
+
+        c.fill = GridBagConstraints.NONE;
+        saveSubPanel.add(savePanel, c);
+    }
+
+    private void initializeSubPanel(JPanel subPanel, JPanel mainPanel) {
+        subPanel.setLayout(new GridBagLayout());
+        GridBagConstraints c = new GridBagConstraints();
+
+        c.fill = GridBagConstraints.NONE;
+        subPanel.add(mainPanel, c);
+    }
+
+    private void initializeSidePanel(JPanel sidePanel, JPanel partialPanel) {
+        sidePanel.setLayout(new BoxLayout(sidePanel, BoxLayout.PAGE_AXIS));
+        sidePanel.add(partialPanel);
 //        directionalButtonAndImagePreviewPanel.add(Box.createVerticalGlue());
         JSeparator separator = new JSeparator();
         separator.setBackground(Color.GRAY);
-        directionalButtonAndImagePreviewPanel.add(Box.createRigidArea(new Dimension(5, 5)));
-        directionalButtonAndImagePreviewPanel.add(separator);
-        directionalButtonAndImagePreviewPanel.add(Box.createRigidArea(new Dimension(5, 5)));
-        directionalButtonAndImagePreviewPanel.add(imagePreviewSubPanel);
-        directionalButtonAndImagePreviewPanel.setBorder(BorderFactory.createEmptyBorder(5, 5, 5, 5));
+        sidePanel.add(Box.createRigidArea(new Dimension(5, 5)));
+        sidePanel.add(separator);
+        sidePanel.add(Box.createRigidArea(new Dimension(5, 5)));
+        sidePanel.add(imagePreviewSubPanel);
+        sidePanel.setBorder(BorderFactory.createEmptyBorder(5, 5, 5, 5));
     }
 
     public MapMakerImagePanel getMapMakerImagePanel() {
