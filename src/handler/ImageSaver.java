5d55573bfceb12fc36737167abf73f87c3b40ffd
Chad Fraser <chad.fraser@mail.mcgill.ca>
2019-07-11 16:35:09 -0600

    Remove unused code

--- src/handler/ImageSaver.java
+++ src/handler/ImageSaver.java
@@ -1,6 +1,5 @@
 package handler;
 
-import java.awt.Image;
 import java.awt.Toolkit;
 import java.awt.datatransfer.Clipboard;
 import java.awt.datatransfer.ClipboardOwner;
@@ -25,70 +24,16 @@ public class ImageSaver implements ClipboardOwner {
         return null;
     }
 
-    public static BufferedImage cropClipboardImage() throws UnsupportedFlavorException, IOException {
-        BufferedImage bufferedImage = getImageFromClipboard();
-        if (bufferedImage != null) {
-            bufferedImage = cropImage(bufferedImage);
-            return bufferedImage;
-        }
-        return null;
-    }
-
     private static BufferedImage getImageFromClipboard() throws UnsupportedFlavorException, IOException {
         Clipboard clipboard = Toolkit.getDefaultToolkit().getSystemClipboard();
         return (BufferedImage) clipboard.getData(DataFlavor.imageFlavor);
     }
 
-//    private void copyImage(BufferedImage bufferedImage) {
-//        TransferableImage transferableImage = new TransferableImage(bufferedImage);
-//        Clipboard clipboard = Toolkit.getDefaultToolkit().getSystemClipboard();
-//        clipboard.setContents(transferableImage, this);
-//    }
-
     private static BufferedImage cropImage(int x, int y, int width, int height, BufferedImage image) {
         return image.getSubimage(x, y, width, height);
-    }
-
-    private static BufferedImage cropImage(BufferedImage image) {
-        return image.getSubimage(8, 67, 512, 384);
     }
 
     public void lostOwnership(Clipboard clipboard, Transferable transferable) {
         System.out.println("Lost Clipboard Ownership");
     }
-
-//    private class TransferableImage implements Transferable {
-//        Image image;
-//
-//        TransferableImage(Image image) {
-//            this.image = image;
-//        }
-//
-//        public Object getTransferData(DataFlavor flavor)
-//                throws UnsupportedFlavorException {
-//            if (flavor.equals(DataFlavor.imageFlavor) && image != null ) {
-//                return image;
-//            }
-//            else {
-//                throw new UnsupportedFlavorException(flavor);
-//            }
-//        }
-//
-//        public DataFlavor[] getTransferDataFlavors() {
-//            DataFlavor[] flavors = new DataFlavor[1];
-//            flavors[0] = DataFlavor.imageFlavor;
-//            return flavors;
-//        }
-//
-//        public boolean isDataFlavorSupported(DataFlavor flavor) {
-//            DataFlavor[] flavors = getTransferDataFlavors();
-//            for (DataFlavor f : flavors) {
-//                if (flavor.equals(f)) {
-//                    return true;
-//                }
-//            }
-//            return false;
-//        }
-//    }
 }
-
